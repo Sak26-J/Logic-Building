5d2c23f59d8d4f2fb2b0d40ed2369ef7a0387f13
Saket Jagtap <83949577+Sak26-J@users.noreply.github.com>
2024-05-08 23:55:34 +0530

    Add files via upload

---
+++ program41.c
@@ -0,0 +1,70 @@
+
+/*
+Accept number as No
+Search all its factors
+Perform addition of all the factors
+If addition is same as No
+then displayas No is perfect number
+otherwise Display as No is not perfect number 
+*/
+
+
+
+
+
+#include<stdio.h>
+#include <stdbool.h>
+
+bool CheckPerfect(int iNo)
+{
+    int iCnt = 0, iSum = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt =1; iCnt <= (iNo/2); iCnt++)
+    {
+        if((iNo % iCnt)== 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    if (iSum == iNo) 
+    {
+        return true;
+    
+    }
+    else {
+    return false;
+    }
+
+}
+
+
+
+
+int main()
+{
+
+    int iValue = 0;
+    bool bRet = false;
+    
+    printf("Enter the number : \n");
+    scanf("%d\n", &iValue);
+
+    bRet = CheckPerfect(iValue);
+    if(bRet == true)
+    {
+        printf("%d is perfect number \n",iValue);
+    }
+    else {
+    printf("%d is not a perfect number \n",iValue);
+    }
+
+
+
+
+    return 0;
+}
---
+++ program49.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+unsigned long int Factorial(int iNo)
+{
+    int iCnt = 0;
+    unsigned long int iFact = 1;
+//       1         2         3
+    for(iCnt =1; iCnt <= iNo; iCnt++)
+    {
+        iFact = iFact * iCnt; // 4
+    }
+    return iFact;
+
+
+}
+int main()
+{
+    int iValue = 0;
+    unsigned long int iRet = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    iRet =Factorial(iValue);
+
+    printf("Factorial is : %lu\n",iRet);
+
+    return 0;
+}
